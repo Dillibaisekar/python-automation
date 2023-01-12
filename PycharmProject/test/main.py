f382241c1469f6cdc9996583b7676c8cd38cce8a
Dillibai S <D516@SVSGLOBAL.COM>
2023-01-12 12:11:59 +0530

    new python project added

---
+++ PycharmProject/test/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome('E:/DRIVER/chromedriver.exe')
+# driver = webdriver.Chrome("E:/DRIVER/chromedriver")
+browser.get("https://www.beta.shriram.com")
+
+
